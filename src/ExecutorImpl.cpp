30c1b8cfb76ec72e0c68d590a9752d0d3f3c98ce
thyrey <625194452@qq.com>
2025-11-29 01:25:49 +0800

    删除ICommand，修改表驱动，实现函数式编程

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -18,40 +18,23 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept:poseHandler(pose)
 
  void ExecutorImpl::Execute(const std::string& commands) noexcept
  {
-    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
-    cmderMap.emplace('M', std::make_unique<MoveCommand>());
-    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
-    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
-    cmderMap.emplace('F', std::make_unique<FastCommand>());
+     std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
+     MoveCommand moveCommand;
+     cmderMap.emplace('M', moveCommand.operate);
+     TurnLeftCommand turnLeftCommand;
+     cmderMap.emplace('L', turnLeftCommand.operate);
+     TurnRightCommand turnRightCommand;
+     cmderMap.emplace('R', turnRightCommand.operate);
+     FastCommand fastCommand;
+     cmderMap.emplace('F', fastCommand.operate);
 
-    for (const auto cmd : commands) {
-        const auto it = cmderMap.find(cmd);
+     for (const auto cmd : commands) {
+         const auto it = cmderMap.find(cmd);
 
-        if (it!= cmderMap.end()) {
-            it->second->DoOperate(poseHandler);
-        }
-    }
-
-
-
-
-    //  for (const auto cmd : commands) {
-    //      std::unique_ptr<ICommand> cmder;
-
-    //      if (cmd == 'M') {
-    //          cmder = std::make_unique<MoveCommand>();
-    //      } else if (cmd == 'L') {
-    //          cmder = std::make_unique<TurnLeftCommand>();
-    //      } else if (cmd == 'R') {
-    //          cmder = std::make_unique<TurnRightCommand>();
-    //      } else if (cmd == 'F') {
-    //          cmder = std::make_unique<FastCommand>();
-    //      } 
-
-    //      if (cmder) {
-    //          cmder->DoOperate(poseHandler);
-    //      }
-    //  }
+         if (it != cmderMap.end()) {
+             it->second(poseHandler);
+         }
+     }
  }
 
  
