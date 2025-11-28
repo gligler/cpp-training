8bcb089dcf10a6f09167129312c8726d8c91bcde
thyrey <625194452@qq.com>
2025-11-29 00:48:57 +0800

    实现应用表驱动std::map消减圈复杂度

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,6 +1,7 @@
 #include "ExecutorImpl.hpp"
 #include "Command.hpp"
 #include <memory>   
+#include <unordered_map>
 
 namespace adas
 {
@@ -17,23 +18,40 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept:poseHandler(pose)
 
  void ExecutorImpl::Execute(const std::string& commands) noexcept
  {
-     for (const auto cmd : commands) {
-         std::unique_ptr<ICommand> cmder;
+    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+    cmderMap.emplace('M', std::make_unique<MoveCommand>());
+    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+    cmderMap.emplace('F', std::make_unique<FastCommand>());
 
-         if (cmd == 'M') {
-             cmder = std::make_unique<MoveCommand>();
-         } else if (cmd == 'L') {
-             cmder = std::make_unique<TurnLeftCommand>();
-         } else if (cmd == 'R') {
-             cmder = std::make_unique<TurnRightCommand>();
-         } else if (cmd == 'F') {
-             cmder = std::make_unique<FastCommand>();
-         } 
+    for (const auto cmd : commands) {
+        const auto it = cmderMap.find(cmd);
 
-         if (cmder) {
-             cmder->DoOperate(poseHandler);
-         }
-     }
+        if (it!= cmderMap.end()) {
+            it->second->DoOperate(poseHandler);
+        }
+    }
+
+
+
+
+    //  for (const auto cmd : commands) {
+    //      std::unique_ptr<ICommand> cmder;
+
+    //      if (cmd == 'M') {
+    //          cmder = std::make_unique<MoveCommand>();
+    //      } else if (cmd == 'L') {
+    //          cmder = std::make_unique<TurnLeftCommand>();
+    //      } else if (cmd == 'R') {
+    //          cmder = std::make_unique<TurnRightCommand>();
+    //      } else if (cmd == 'F') {
+    //          cmder = std::make_unique<FastCommand>();
+    //      } 
+
+    //      if (cmder) {
+    //          cmder->DoOperate(poseHandler);
+    //      }
+    //  }
  }
 
  
